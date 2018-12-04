fd0a778eea84cd28faf1a6ea044385a5f33206eb
Kamesh Mahendrawada <kameshsaim@gmail.com>
2018-12-04 10:21:51 +0530

    Date format

--- HotelFlexProject/app/static/app/js/valid-date.js
+++ HotelFlexProject/app/static/app/js/valid-date.js
@@ -13,6 +13,10 @@ function handler(e) {
     var dd = today.getDate();
     var mm = today.getMonth() + 1; //January is 0!
     var yyyy = today.getFullYear();
+    if(dd<10)
+    {
+        dd = '0'+dd
+    }
     today = mm + '/' + dd + '/' + yyyy;
     selected = e.target.value;
     if (selected < today) {
